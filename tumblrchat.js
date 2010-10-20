f23b069d3450f8388bbf0366fb6a69134566527c
Kevin Nuut <tumblrchat@kevinnuut.com>
2010-10-20 13:18:24 -0500

    Adding core files, next is submodules

--- tumblrchat.js
+++ tumblrchat.js
@@ -1,1 +1,41 @@
-alert('test');
+var http = require('http'), 
+    url  = require('url'),
+    fs   = require('fs'),
+    io   = require('./'),
+    sys  = require('sys'),
+
+    server = http.createServer(function(request, response)
+    {
+        
+    }),
+
+    send404 = function(res)
+    {
+        res.writeHead(404);
+        res.write('404');
+        res.end();
+    };
+
+server.listen(8080);
+		
+// socket.io, I choose you
+// simplest chat application evar
+var socket = io.listen(server),
+    buffer = [];
+		
+socket.on('connection', function(client)
+{
+    client.send({ buffer: buffer });
+    client.broadcast({ announcement: client.sessionId + ' connected' });
+
+    client.on('message', function(message){
+            var msg = { message: [client.sessionId, message] };
+            buffer.push(msg);
+            if (buffer.length > 15) buffer.shift();
+            client.broadcast(msg);
+    });
+
+    client.on('disconnect', function(){
+            client.broadcast({ announcement: client.sessionId + ' disconnected' });
+    });
+});
