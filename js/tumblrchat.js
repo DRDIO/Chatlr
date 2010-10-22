064a75af6f43049166d65b50a282738598d66a08
Kevin Nuut <tumblrchat@kevinnuut.com>
2010-10-22 13:46:12 -0500

    follow me

--- js/tumblrchat.js
+++ js/tumblrchat.js
@@ -181,10 +181,11 @@ $(function() {
                 lastMessage   = message;
                 lastTimestamp = timestamp;
 
-                // I hate follow me requests
+                // I hate follow me requests, and dumb questions
                 message = message.replace(/follow (me)?/i, "...I am lonely...");
                 message = message.replace(/what is this\??/i, "...This is Tumblr Chat...");
                 message = message.replace(/is this (for )?real\??/i, "...Tumblr Chat is real...");
+                message = message.replace(/(did )?(who|tumblr) made this\??/i, "...Kevin Nuut made this...");
 
                 // I hate similar charactesr in a row
                 message = message.replace(/(.+?)\1{3,}/g, '$1');
