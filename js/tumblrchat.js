df26f8901cebf1b842c5a2deee84167b417306b6
Kevin Nuut <tumblrchat@kevinnuut.com>
2010-10-21 19:49:59 -0500

    stop loop

--- js/tumblrchat.js
+++ js/tumblrchat.js
@@ -6,6 +6,12 @@ $.fn.dotdotdot = function() {
     });
 };
 
+$.fn.dotdotend = function() {
+    this.each(function() {
+        $(this).stop(true).fadeTo(5000, 0.1);
+    });
+};
+
 var socket;
 
 $(function() {
@@ -14,162 +20,180 @@ $(function() {
     
     // Initialize variables
     var clientId,
-        users;
+        users,
+        lastTimestamp = 0,
+        lastMessage   = '';
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
     // Connect to socket server
-    socket = new io.Socket(null, {port: 8080, transports: ['websocket']});
-    socket.connect();
+    if (typeof io == 'undefined') {
+        notifyFailure(false);
+    } else {
 
-    setTimeout("notifyFailure()", socket.options.connectTimeout);
+        socket = new io.Socket(null, {port: 8080, transports: ['websocket']});
+        socket.connect();
 
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+        setTimeout("notifyFailure(true)", socket.options.connectTimeout);
 
-    // EVENT: When receiving a message from the server
-    socket.on('message', function(serverRes)
-    {        
-        // Only accept messages with type property
-        if ('type' in serverRes && serverRes.type in {'init': '', 'message': '', 'status': ''}) {
-            // First message sent from server, initalize chat
-            if (serverRes.type == 'init' && 'id' in serverRes && 'users' in serverRes) {
-                // Initialize self user with php vars
-                clientId = serverRes.id;
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-                var selfCreds = {
-                    title:  tumblrTitle,
-                    name:   tumblrName,
-                    url:    tumblrUrl,
-                    avatar: tumblrAvatar};
+        // EVENT: When receiving a message from the server
+        socket.on('message', function(serverRes)
+        {
+            // Only accept messages with type property
+            if ('type' in serverRes && serverRes.type in {'init': '', 'message': '', 'status': ''}) {
+                // First message sent from server, initalize chat
+                if (serverRes.type == 'init' && 'id' in serverRes && 'users' in serverRes) {
+                    // Initialize self user with php vars
+                    clientId = serverRes.id;
 
-                // On init, a list of users is grabbed (and add yourself)
-                users = serverRes.users;
-                users[clientId] = selfCreds;
+                    var selfCreds = {
+                        title:  tumblrTitle,
+                        name:   tumblrName,
+                        url:    tumblrUrl,
+                        avatar: tumblrAvatar};
 
-                // Send a connect message with credentials
-                selfCreds.type = 'credentials';
-                socket.send(selfCreds);
+                    // On init, a list of users is grabbed (and add yourself)
+                    users = serverRes.users;
+                    users[clientId] = selfCreds;
 
-                // Add users to the chat list
-                var userCount = 0;
-                for (var i in users) {
-                    displayUser(i);
-                    userCount++;
+                    // Send a connect message with credentials
+                    selfCreds.type = 'credentials';
+                    socket.send(selfCreds);
+
+                    // Add users to the chat list
+                    var userCount = 0;
+                    for (var i in users) {
+                        displayUser(i);
+                        userCount++;
+                    }
+
+                    // Update user count up top
+                    $('#count').text(userCount);
+
+                    // Output buffer messages
+                    for (var i in serverRes.buffer) {
+                        displayMessage(serverRes.buffer[i]);
+                    }
+
+                    // Update status to say they joined
+                    var joinedRes = {
+                        type:    'status',
+                        user:    users[clientId],
+                        message: 'joined the chat!'};
+                    displayMessage(joinedRes);
+
+                    $('#loading').fadeOut(1000);
+
+                // If a new user is coming or going, update list accordingly
+                } else if (serverRes.type == 'status' && 'mode' in serverRes && serverRes.mode in {'connect': '', 'disconnect': ''} && 'id' in serverRes) {
+                    // new user joined, let's add to user list!
+                    if (serverRes.mode == 'connect' && 'user' in serverRes) {
+                        users[serverRes.id] = serverRes.user;
+                        $('#count').text(1 + parseInt($('#count').text()));
+                        displayUser(serverRes.id);
+                        if (parseInt($('#count').text()) < 20) {
+                            displayMessage(serverRes);
+                        }
+
+                    // Awh, a user left, let's remove from user list
+                    } else if (serverRes.mode == 'disconnect') {
+                        // Pull users from local list and display disconnect
+                        serverRes.user = users[serverRes.id];
+                        if (parseInt($('#count').text()) < 20) {
+                            displayMessage(serverRes);
+                        }
+
+                        // Remove local user
+                        $('#count').text(-1 + parseInt($('#count').text()));
+                        removeUser(serverRes.id);
+                        delete users[serverRes.id];
+                    }
+
+                // Otherwise, process whatever message or generic status comes in
+                } else if (serverRes.type == 'message' || serverRes.type == 'status') {
+                    // Pull users from local array and display message or status
+                    serverRes.user = users[serverRes.id];
+                    displayMessage(serverRes);
                 }
+            }
+        });
 
-                // Update user count up top
-                $('#count').text(userCount);
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-                // Output buffer messages
-                for (var i in serverRes.buffer) {
-                    displayMessage(serverRes.buffer[i]);
-                }
+        // EVENT: If mouse is down, prevent scroll from moving to bottom of screen
+        $(window).mousedown(function(e) {
+           mouseDown = true;
+        });
 
-                // Update status to say they joined
-                var joinedRes = {
+        // EVENT: If mouse is up, scrolling is cool
+        $(window).mouseout(function(e) {
+            mouseDown = false;
+        });
+
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+        // EVENT: On resize, move to bottom of the screen
+        $(window).resize(function(e) {
+            $('#chat').scrollTop($('#chat')[0].scrollHeight);
+        });
+
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+        $('.topbuttons').click(function(e) {
+            e.preventDefault();
+
+            var message = 'This feature will be available in the next version!';
+            if ($(this).attr('rel')) {
+                message = $('#' + $(this).attr('rel')).html();
+            }
+
+            $('<div/>')
+                .attr('title', $(this).attr('title'))
+                .dialog({
+                    width: '40%',
+                    minWidth: '320px',
+                    resizable: false})
+                .html(message)
+                .parent().position({my: 'center', at: 'center', of: document});
+        });
+
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+        // EVENT: When the text box is submitted, prevent submission and send message
+        $('#form').submit(function(e) {
+            e.preventDefault();
+            var message   = $('#text').val();
+            var timestamp = new Date().getTime();
+
+            if (message == lastMessage || timestamp - lastTimestamp < 2500) {
+                // Quickly display message to self in pink
+                displayMessage({
                     type:    'status',
                     user:    users[clientId],
-                    message: 'joined the chat!'};
-                displayMessage(joinedRes);
+                    message: 'Ignored as spam due to repetition or frequency.'});
+            } else {
+                lastMessage   = message;
+                lastTimestamp = timestamp;
 
-                $('#loading').fadeOut(1000);
+                // Send to server for broadcast
+                socket.send({
+                    type: 'message',
+                    message: message});
 
-            // If a new user is coming or going, update list accordingly
-            } else if (serverRes.type == 'status' && 'mode' in serverRes && serverRes.mode in {'connect': '', 'disconnect': ''} && 'id' in serverRes) {
-                // new user joined, let's add to user list!
-                if (serverRes.mode == 'connect' && 'user' in serverRes) {
-                    users[serverRes.id] = serverRes.user;
-                    $('#count').text(1 + parseInt($('#count').text()));
-                    displayUser(serverRes.id);
-                    if (parseInt($('#count').text()) < 20) {
-                        displayMessage(serverRes);
-                    }
-                    
-                // Awh, a user left, let's remove from user list
-                } else if (serverRes.mode == 'disconnect') {
-                    // Pull users from local list and display disconnect
-                    serverRes.user = users[serverRes.id];
-                    if (parseInt($('#count').text()) < 20) {
-                        displayMessage(serverRes);
-                    }
+                // Quickly display message to self in pink
+                displayMessage({
+                    type:    'personal',
+                    user:    users[clientId],
+                    message: message});
 
-                    // Remove local user
-                    $('#count').text(-1 + parseInt($('#count').text()));
-                    removeUser(serverRes.id);
-                    delete users[serverRes.id];
-                }                
-
-            // Otherwise, process whatever message or generic status comes in
-            } else if (serverRes.type == 'message' || serverRes.type == 'status') {
-                // Pull users from local array and display message or status
-                serverRes.user = users[serverRes.id];
-                displayMessage(serverRes);
+                // Clear text box
+                $('#text').val('');
             }
-        }
-    });
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
-    // EVENT: If mouse is down, prevent scroll from moving to bottom of screen
-    $(window).mousedown(function(e) {
-       mouseDown = true;
-    });
-
-    // EVENT: If mouse is up, scrolling is cool
-    $(window).mouseout(function(e) {
-        mouseDown = false;
-    });
+        });
+    }
     
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
-    // EVENT: On resize, move to bottom of the screen
-    $(window).resize(function(e) {
-        $('#chat').scrollTop($('#chat')[0].scrollHeight);
-    });
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
-    $('.topbuttons').click(function(e) {
-        e.preventDefault();
-
-        var message = 'This feature will be available in the next version!';
-        if ($(this).attr('rel')) {
-            message = $('#' + $(this).attr('rel')).html();
-        }
-
-        $('<div/>')
-            .attr('title', $(this).attr('title'))
-            .dialog({
-                width: '40%',
-                minWidth: '320px',
-                resizable: false})
-            .html(message)
-            .parent().position({my: 'center', at: 'center', of: document});
-    });
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
-    // EVENT: When the text box is submitted, prevent submission and send message
-    $('#form').submit(function(e) {
-        e.preventDefault();
-
-        var message = $('#text').val();
-
-        // Send to server for broadcast
-        socket.send({
-            type: 'message',
-            message: message});
-
-        // Quickly display message to self in pink
-        displayMessage({
-            type:    'personal',
-            user:    users[clientId],
-            message: message});
-
-        // Clear text box
-        $('#text').val('');
-    });
-
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
     // FUNCTION: Display various message types to the screen
@@ -252,10 +276,11 @@ $(function() {
     }
 });
 
-function notifyFailure()
+function notifyFailure(hasSocket)
 {
-    if (!socket.connecting && !socket.connected) {
-        $('#loading-pulse').stop();
+    if (!hasSocket || (!socket.connecting && !socket.connected)) {
+        $('#loading-pulse').dotdotend();
+        
         $('title').text('Tumblr Chat (Error!)');
 
         $('<div/>')
