1e047aa03b5008b1701354648dc93a3d6edb3cf0
Kevin Nuut <tumblrchat@kevinnuut.com>
2010-11-03 14:12:17 -0500

    socket connections

--- website/js/tumblrchat.js
+++ website/js/tumblrchat.js
@@ -284,8 +284,8 @@ $(function() {
                             $(this).dialog('close');
                         }
                     },
-                    width: '90%',
-                    maxWidth: '320px',
+                    width: $(window).width() * 0.8,
+                    maxWidth: '320',
                     resizable: false})
                 .html('<em>' + url + '</em> You are about to open an external link that might be offensive or contain viruses. Do you still want to visit it?')
                 .parent().position({my: 'center', at: 'center', of: document});
