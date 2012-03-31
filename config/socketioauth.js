b87b7259ce98822f42403e9fb4ac2f0a25d51108
Kevin Nuut <sayhello@kevinnuut.com>
2012-03-31 16:14:03 +0000

    changes

--- config/socketioauth.js
+++ config/socketioauth.js
@@ -1,19 +1,19 @@
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 // Configuration Variables, YAY!
 //
-exports.host    = null;
-exports.port    = 8080;
+exports.ipaddr   = '0.0.0.0';
+exports.port     = process.env.C9_PORT;
 exports.secret  = 'sauce';
 
 exports.protocol        = 'http://';
-exports.domain          = 'tumblrchat.loc:8080';
+exports.domain          = 'chatlr.kevinnuut.c9.io';
 exports.requestUrl      = 'http://www.tumblr.com/oauth/request_token';
 exports.accessUrl       = 'http://www.tumblr.com/oauth/access_token';
 exports.authorizeUrl    = 'http://www.tumblr.com/oauth/authorize';
 exports.authenticateUrl = 'http://api.tumblr.com/v2/user/info';
 
-exports.consumerKey    = 'svbT4DJfa0G1LDo4BkFlxPhlshIIYzrOoE1IWieuww0fS07P21';
-exports.consumerSecret = 'TpF7X2GIWV2duq7g2sHijZ64QMNtvfB7i050zyiDvkhw6aC8Sk';
+exports.consumerKey    = 'BgIkIo9ReF5A5VSFNwNmtY8EHoYqw3KWA3ScKxCMHHNxi0DLxm';
+exports.consumerSecret = 'z7FMJmNa34wHsokEfyiK1OCFArY1KWX1MprmXz1uMjJ6OG9P4A';
 exports.callbackUrl    = exports.protocol + exports.domain + '/callback';
 
 exports.websitePath    = __dirname + '/../website';
