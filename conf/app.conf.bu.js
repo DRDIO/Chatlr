6e07c23c220a47b5e3d38ccb3ca712950814c8e8
Kevin Nuut <kevin+github@drd.io>
2012-04-22 20:55:02 -0500

    conf

--- conf/app.conf.bu.js
+++ conf/app.conf.bu.js
@@ -7,26 +7,17 @@ exports.timerList = {
 
 exports.dft = {
     room:  'main',
-    owner: 'kevinnuut'
-};
-
-exports.opList = {
-    'kevinnuut':     0,
-    'lacey':         1,
-    'gompr':         1,
-    'topherchris':   1,
-    'brittanyforks': 1,
-    'fajita':        1
+    owner: ''
 };
 
 exports.db = {
-    'database': 'chatlrdev'
+    'database': 'chat'
 };
 
 exports.server = {
     host:       '',
     port:       8080,
-    secret:     'sauce1!',
+    secret:     'XXXX',
     publicPath: __dirname + '/../website'
 };
 
@@ -39,12 +30,13 @@ exports.oauth = {
     authorizeUrl:    'http://www.tumblr.com/oauth/authorize',
     authenticateUrl: 'http://api.tumblr.com/v2/user/info',
 
-    consumerKey:     'svbT4DJfa0G1LDo4BkFlxPhlshIIYzrOoE1IWieuww0fS07P21',
-    consumerSecret:  'TpF7X2GIWV2duq7g2sHijZ64QMNtvfB7i050zyiDvkhw6aC8Sk',
-    callbackUrl:     'http://tumblrchat.loc:8080/callback',
+    consumerKey:     'XXXX',
+    consumerSecret:  'XXXX',
 
     appHtmlPath:     exports.server.publicPath + '/content.html'
 };
+
+exports.oauth.callbackUrl = exports.oauth.protocal + exports.oauth.domain + '/callback';
 
 exports.socketio = {
     logLevel: 2
